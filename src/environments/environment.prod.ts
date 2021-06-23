2bd5cb5bec282d65f7a383bb8a5c3172df27a7b0
mariaelena9 <maelmontanezhe@ittepic.edu.mx>
2021-06-22 18:33:37 -0600

    Produccion y detalles

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyAy7VU7fbKACedFJjlKSNnfwo_YYePMtnk",
+    authDomain: "sweetstore-be498.firebaseapp.com",
+    databaseURL: "https://sweetstore-be498-default-rtdb.firebaseio.com",
+    projectId: "sweetstore-be498",
+    storageBucket: "sweetstore-be498.appspot.com",
+    messagingSenderId: "288797234145",
+    appId: "1:288797234145:web:b97841860dcb2227b138a0"
+  }
+};
